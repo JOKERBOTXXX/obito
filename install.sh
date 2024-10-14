8c486a3a26daa873754b97fe23705fdfb2ac204d
JOKERBOTXXX <nazimop927241@gmail.com>
2024-10-14 19:25:19 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python /workspaces/Hacker/K3.py",
+    "postStartCommand": "python /workspaces/obito/K3.py",
     "customizations": {
         "vscode": {
             "settings": {
